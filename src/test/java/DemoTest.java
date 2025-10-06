1d150aa0e9a17d6d5120f36230b5bf869b6f18b9
Devansh <157308124+devanshsingh15@users.noreply.github.com>
2025-10-06 16:49:46 +0530

    Update DemoTest.java

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,10 +1,15 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class DemoTest {
     WebDriver driver;
@@ -37,4 +42,3 @@ public class DemoTest {
 
 
 }
-
